4b37cafca8e184ec8e7cc5849c001ad28cd4af54
Boiko Yaroslav <y.boiko@easyget.com.ua>
2021-03-08 00:38:39 +0200

    add header, footer, about section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,17 @@
 import * as React from "react";
 import IntroductionSection from "../components/Landing/IntroductionSection";
-import ProcessSection from "../components/Landing/ProcessSection";
 import "../styles/main.sass";
+import BaseLayout from "../components/Layout";
+import AboutSection from "../components/Landing/AboutSection";
+import ProcessSection from "../components/Landing/ProcessSection";
 
 const IndexPage = () => {
   return (
-    <main>
+    <BaseLayout>
       <IntroductionSection />
+      <AboutSection />
       <ProcessSection />
-    </main>
+    </BaseLayout>
   );
 };
 
