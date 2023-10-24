a7f6bfe0e55046a7f32be3b100b091b9ae08d82c
abbze <abbzew@gmail.com>
2023-10-24 21:48:14 +0400

    gatsby-config.js > gatsby-config.ts

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,7 +1,9 @@
-const config = {
+import type { GatsbyConfig } from 'gatsby'
+
+const config: GatsbyConfig = {
   siteMetadata: {
-    title: 'argine',
-    siteUrl: 'https://argine.digital/',
+    title: `argine`,
+    siteUrl: 'https://atgine.com.ua/',
     description: 'argine description',
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
