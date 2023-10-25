859dffe8c7ad2cd876adf6e6b864c0aefd84e453
abbze <abbzew@gmail.com>
2023-10-25 16:33:29 +0400

    add favicon

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -26,20 +26,21 @@ const config: GatsbyConfig = {
       __key: 'images',
     },
     {
-      resolve: 'gatsby-plugin-favicons',
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        logo: './src/images/favicon.png',
-        appName: 'argine.digital',
-        background: '#1A1A1A',
-        icons: {
-          android: true,
-          appleIcon: true,
-          appleStartup: true,
-          coast: false,
-          favicons: true,
-          yandex: false,
-          windows: false,
-        },
+        name: "argine.digital",
+        short_name: "argine",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/favicon.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: 'use-credentials',
       },
     },
   ],
