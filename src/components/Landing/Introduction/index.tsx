710213182e5e69febc5a0926e49d032dcc5703e0
abbze <abbzew@gmail.com>
2024-01-08 21:26:19 +0400

    replace video

--- src/components/Landing/Introduction/index.tsx
+++ src/components/Landing/Introduction/index.tsx
@@ -1,21 +1,22 @@
-import React, { FC } from 'react'
 import { AdvancedVideo } from '@cloudinary/react'
-import { CloudinaryVideo } from '@cloudinary/url-gen'
-import { scale } from '@cloudinary/url-gen/actions/resize'
-import { brightness } from '@cloudinary/url-gen/actions/adjust'
 import { auto, autoBest } from '@cloudinary/transformation-builder-sdk/qualifiers/quality'
-import { isMobile } from 'react-device-detect'
+import { CloudinaryVideo } from '@cloudinary/url-gen'
+import { brightness } from '@cloudinary/url-gen/actions/adjust'
 import { quality } from '@cloudinary/url-gen/actions/delivery'
+import { scale } from '@cloudinary/url-gen/actions/resize'
+import React from 'react'
+import { isMobile } from 'react-device-detect'
 
-const IntroductionSection: FC = () => {
+const IntroductionSection: React.FC = () => {
   // Constants
   const videoId = 'v1697899631/pexels_videos_1851190_2160p_gu7jev'
-  const videoDesktopInstanse = new CloudinaryVideo(videoId, { cloudName: 'dwy6v0hix' })
-  const videoMobileInstanse = new CloudinaryVideo(videoId, { cloudName: 'dwy6v0hix' })
+  const newVideoId = 'v1704728827/video_2024-01-08_19-46-41_n3kiob'
+  const videoDesktopInstanse = new CloudinaryVideo(newVideoId, { cloudName: 'dwy6v0hix' })
+  const videoMobileInstanse = new CloudinaryVideo(newVideoId, { cloudName: 'dwy6v0hix' })
 
   const videoDesktopEdited = videoDesktopInstanse
     .resize(scale().width(1920))
-    .adjust(brightness(-10))
+    //.adjust(brightness(-10))
     .quality(auto())
     .delivery(quality(auto()))
   const videoMobileEdited = videoMobileInstanse
@@ -33,7 +34,7 @@ const IntroductionSection: FC = () => {
 
         <div className="introduction__animation">
           <AdvancedVideo
-            style={{ height: '100vh', overflow: 'hidden' }}
+            style={{ overflow: 'hidden' }}
             cldVid={isMobile ? videoMobileEdited : videoDesktopEdited}
             cldPoster="auto"
             autoPlay
